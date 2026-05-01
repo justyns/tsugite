33bf3fb0dccd8ec75f5f307fa49c95213652861a
Justyn Shull <git@justyn.io>
2026-05-01 02:25:33 -0500

    fix: read __version__ from package metadata

--- tsugite/__init__.py
+++ tsugite/__init__.py
@@ -3,8 +3,13 @@
 import logging
 import os
 import sys
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _pkg_version
 
-__version__ = "0.1.0"
+try:
+    __version__ = _pkg_version("tsugite-cli")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
 
 # Configure logging to stderr (keep stdout clean for piping)
 _log_level = os.environ.get("TSUGITE_LOG_LEVEL", "WARNING").upper()
